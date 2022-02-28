6f0747375f4d7652627f8ad8b1c55153cb500f2e
mohanraj-exe <95164416+mohanraj-exe@users.noreply.github.com>
2022-02-28 06:07:38 -0800

    Add files via upload

---
+++ script_capital.js
@@ -0,0 +1,22 @@
+//API-JSON parsing via jsCode
+
+//1.Creating request object
+var request = new XMLHttpRequest();
+
+//2.Open my request connection
+request.open('GET','https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json');
+
+//3.Send a request
+request.send();
+
+//4.Data after response
+request.onload = function(){
+    var data = JSON.parse(this.response);
+    // for(i in data){
+    //     console.log(data[i].name)
+    // }
+    data.forEach(name)
+    function name(arr){
+        console.log(arr.capital)
+    }
+}

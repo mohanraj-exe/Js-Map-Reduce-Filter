09df63eecbc0827bd7627e661577af792f1816d8
mohanraj-exe <95164416+mohanraj-exe@users.noreply.github.com>
2022-02-28 06:13:30 -0800

    Add files via upload

---
+++ script_population.js
@@ -0,0 +1,17 @@
+//API-JSON parsing via jsCode
+
+//1.Creating request object
+var request = new XMLHttpRequest();
+
+//2.Open my request connection
+request.open('GET','https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json');
+
+//3.Send a request
+request.send();
+
+//4.Data after response
+request.onload = function(){
+    var data = JSON.parse(this.response);
+    var res = data.filter(ele => ele.population < 200000)
+    console.log(res)
+}
